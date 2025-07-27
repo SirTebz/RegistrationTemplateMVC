114b0a7db8461bfd4f28b3cf16d402445fcfce93
Teboho Mokgosi <62904711+SirTebz@users.noreply.github.com>
2025-07-27 15:51:24 +0200

    Add project files.

---
+++ RegistrationTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegistrationTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
